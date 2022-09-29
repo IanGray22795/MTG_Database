5074c1d8c66ff1a82e33951f529485b5f8885f15
Ian Gray <gra18005@byui.edu>
2022-09-29 12:50:50 -0600

    started construction of database in R

--- duckdb database/database loader.r
+++ duckdb database/database loader.r
@@ -4,10 +4,15 @@ install.packages("tidyverse")
 library("DBI")
 library("tidyverse")
 
-Magic_DB = dbConnect(duckdb::duckdb())
+Magic_DB = dbConnect(duckdb::duckdb(), dbdir="MTG_Card.db", read_only = FALSE)
 
+#table construction
+dbExecute(Magic_DB, "CREATE OR REPLACE TABLE set(id INTEGER PRIMARY KEY, Name VARCHAR, Release_Date DATE)")
 
- #table construction
- 
+dbExecute(Magic_DB, "INSERT INTO set VALUES(1,'Alpha', '1993-08-05')")
 
-dbExecute(Magic_DB, "CREATE TABLE items")
+MTG_view = dbGetQuery(Magic_DB, "SELECT * FROM set")
+
+View(MTG_view)
+
+dbDisconnect(Magic_DB, shutdown = TRUE)
