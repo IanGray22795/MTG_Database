fa5ddf4e42434912fb58b7a4c4013f5f6a8cc2a5
IanGray22795 <ian.m.gray27@gmail.com>
2022-09-30 21:05:33 -0600

    r data insertion changes

--- duckdb database/database loader.r
+++ duckdb database/database loader.r
@@ -4,16 +4,20 @@ install.packages("tidyverse")
 library("DBI")
 library("tidyverse")
 
-Magic_DB =dbConnect(duckdb::duckdb(), dbdir="MTG_Card.duckdb", read_only = FALSE)
+Magic_DB <- dbConnect(duckdb::duckdb(), dbdir = "MTG_Card.duckdb", read_only = FALSE)
 
 #table construction
 
-dbExecute(Magic_DB, "CREATE OR REPLACE TABLE set(id INTEGER PRIMARY KEY, Name VARCHAR, Release_Date DATE)")
+dbExecute(Magic_DB, "CREATE OR REPLACE TABLE set(id INTEGER PRIMARY KEY, 
+                                                 Name VARCHAR, 
+                                                 Release_Date DATE)")
 
-dbExecute(Magic_DB, "INSERT INTO set VALUES (1,'Alpha', '1993-08-05'), (2, 'Beta', '1993-10-04'), (3, 'Unlimited', '1993-12-01')")
+dbExecute(Magic_DB, "INSERT INTO set VALUES (1,'Alpha', '1993-08-05'), 
+                                            (2, 'Beta', '1993-10-04'), 
+                                            (3, 'Unlimited', '1993-12-01')")
 
-MTG_view = dbGetQuery(Magic_DB, "SELECT * FROM set")
+MTG_View <- dbGetQuery(Magic_DB, "SELECT * FROM set")
 
-View(MTG_view)
+View(MTG_View)
 
-dbDisconnect(Magic_DB, shutdown=TRUE)
+dbDisconnect(Magic_DB, shutdown = TRUE)
