22c19e0507f22e9c03fbe23b49f2b389e2673552
IanGray22795 <ian.m.gray27@gmail.com>
2022-10-02 21:35:19 -0600

    connection between py andr files to db fixed

--- duckdb database/database loader.py
+++ duckdb database/database loader.py
@@ -2,11 +2,12 @@
 import duckdb
 import pandas as pd
 # %%
-con = duckdb.connect()
-
-#con.execute("SELECT * FROM set")
-
+con = duckdb.connect(database = "../MTG_Card.duckdb", read_only=False)
+# %%
+con.execute("SELECT * FROM set")
+print(con.fetchall())
 
 # %%
 
 con.close()
+# %%
