b13075c7233837ba4bbc9d9df6ff6460fa1fb292
Ian Gray <gra18005@byui.edu>
2022-10-10 19:54:59 -0600

    added card table to database

--- duckdb_database/database loader.r
+++ duckdb_database/database loader.r
@@ -11,10 +11,14 @@ Magic_DB <- dbConnect(duckdb::duckdb(), dbdir = "MTG_Card.duckdb", read_only = F
 dbExecute(Magic_DB, "CREATE OR REPLACE TABLE set(id INTEGER PRIMARY KEY, 
                                                  Name VARCHAR, 
                                                  Release_Date DATE)")
+dbExecute(Magic_DB, "Create OR RELACE TABLE card(id INTEGER PRIMARY KEY, 
+                                                 Name VARCHAR")
 
 dbExecute(Magic_DB, "INSERT INTO set VALUES (1,'Alpha', '1993-08-05'), 
                                             (2, 'Beta', '1993-10-04'), 
                                             (3, 'Unlimited', '1993-12-01')")
+
+
 
 MTG_View <- dbGetQuery(Magic_DB, "SELECT * FROM set")
 
