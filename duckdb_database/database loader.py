90aadba868097bccfa1b88f1bff594acd0b8a0ec
IanGray22795 <ian.m.gray27@gmail.com>
2022-10-19 14:56:49 -0600

    tried to load in json files

--- duckdb_database/database loader.py
+++ duckdb_database/database loader.py
@@ -1,13 +1,29 @@
-# %%
+#%%
 import duckdb
+# import numpy as np
+from pyarrow import json
+# import SparkSession as ps
 import pandas as pd
-# %%
-con = duckdb.connect(database = "../MTG_Card.duckdb", read_only=False)
-# %%
-con.execute("SELECT * FROM set")
-print(con.fetchall())
 
-# %%
+#%%
 
-con.close()
-# %%
+# con = duckdb.connect(database = "../MTG_Card.duckdb", read_only=False)
+
+#%%
+
+# con.execute("SELECT * FROM set")
+# print(con.fetchall())
+
+#%%
+
+# con.close()
+
+#%%
+
+df = json.read_json("C:/Users/Ian Gray/Documents/GitHub/github repositories/MTG_Database/json_data/setlist.json")
+
+setlist.head(10)
+
+#%%
+
+
--- duckdb_database/database loader.r
+++ duckdb_database/database loader.r
@@ -8,19 +8,26 @@ Magic_DB <- dbConnect(duckdb::duckdb(), dbdir = "MTG_Card.duckdb", read_only = F
 
 #table construction
 
-dbExecute(Magic_DB, "CREATE OR REPLACE TABLE set(id INTEGER PRIMARY KEY, 
-                                                 Name VARCHAR, 
-                                                 Release_Date DATE)")
-dbExecute(Magic_DB, "Create OR RELACE TABLE card(id INTEGER PRIMARY KEY, 
-                                                 Name VARCHAR")
+# dbExecute(Magic_DB, "CREATE OR REPLACE TABLE set(id INTEGER PRIMARY KEY, 
+#                                                  Name VARCHAR, 
+#                                                  Release_Date DATE)")
 
-dbExecute(Magic_DB, "INSERT INTO set VALUES (1,'Alpha', '1993-08-05'), 
-                                            (2, 'Beta', '1993-10-04'), 
-                                            (3, 'Unlimited', '1993-12-01')")
+# dbExecute(Magic_DB, "CREATE UNIQUE INDEX id ON set(id)")
+
+#transforming json file into table
+
+dbExecute(Magic_DB, "json(")
+
+# dbExecute(Magic_DB, "Create OR REPLACE TABLE card(id INTEGER PRIMARY KEY, 
+#                                                  Name VARCHAR")
+
+# dbExecute(Magic_DB, "INSERT INTO set VALUES (1,'Alpha', '1993-08-05'), 
+#                                             (2, 'Beta', '1993-10-04'), 
+#                                             (3, 'Unlimited', '1993-12-01')")
 
 
 
-MTG_View <- dbGetQuery(Magic_DB, "SELECT * FROM set")
+MTG_View <- dbGetQuery(Magic_DB, "SELECT json_structure(j) FROM setlist")
 
 View(MTG_View)
 
