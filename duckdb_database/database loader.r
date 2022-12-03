1673368250fe4a13960369ad65ee3c355dbc239c
IanGray22795 <ian.m.gray27@gmail.com>
2022-12-03 16:56:18 -0700

    commit changes

--- duckdb_database/database loader.r
+++ duckdb_database/database loader.r
@@ -1,43 +1,24 @@
 
 install.packages('duckdb', repos=c('https://duckdb.r-universe.dev', 'https://cloud.r-project.org'))
 install.packages("tidyverse")
+
 library("DBI")
 library("tidyverse")
 
 Magic_DB <- dbConnect(duckdb::duckdb(), dbdir = ":memory:", read_only = FALSE)
 
-dbExecute(Magic_DB, "CREATE OR REPLACE TABLE Cards AS SELECT * FROM read_csv_auto('..\\data\\sqllite_data\\AllPrintingsCSVFiles\\cards.csv',SAMPLE_SIZE=-1);")
-dbExecute(Magic_DB, "CREATE OR REPLACE TABLE Legalities AS SELECT * FROM read_csv_auto('..\\data\\sqllite_data\\AllPrintingsCSVFiles\\legalities.csv',SAMPLE_SIZE=-1);")
-dbExecute(Magic_DB, "CREATE OR REPLACE TABLE Meta AS SELECT * FROM read_csv_auto('..\\data\\sqllite_data\\AllPrintingsCSVFiles\\meta.csv',SAMPLE_SIZE=-1);")
-dbExecute(Magic_DB, "CREATE OR REPLACE TABLE Rulings AS SELECT * FROM read_csv_auto('..\\data\\sqllite_data\\AllPrintingsCSVFiles\\rulings.csv',SAMPLE_SIZE=-1);")
-dbExecute(Magic_DB, "CREATE OR REPLACE TABLE Set_translations AS SELECT * FROM read_csv_auto('..\\data\\sqllite_data\\AllPrintingsCSVFiles\\set_translations.csv',SAMPLE_SIZE=-1);")
-dbExecute(Magic_DB, "CREATE OR REPLACE TABLE Sets AS SELECT * FROM read_csv_auto('..\\data\\sqllite_data\\AllPrintingsCSVFiles\\sets.csv',SAMPLE_SIZE=-1);")
-dbExecute(Magic_DB, "CREATE OR REPLACE TABLE Tokens AS SELECT * FROM read_csv_auto('..\\data\\sqllite_data\\AllPrintingsCSVFiles\\tokens.csv',SAMPLE_SIZE=-1);")
-# dbExecute(Magic_DB, "CREATE OR REPLACE TABLE set(id INTEGER PRIMARY KEY, 
-#                                                  Name VARCHAR, 
-#                                                  Release_Date DATE)")
+dbExecute(Magic_DB, "CREATE SCHEMA IF NOT EXISTS MTG;")
+dbExecute(Magic_DB, "CREATE OR REPLACE TABLE MTG.Cards AS SELECT * FROM read_csv_auto('..\\MTG_Database\\data\\sqllite_data\\AllPrintingsCSVFiles\\cards.csv',SAMPLE_SIZE=-1);")
+dbExecute(Magic_DB, "CREATE OR REPLACE TABLE MTG.Legalities AS SELECT * FROM read_csv_auto('..\\MTG_Database\\data\\sqllite_data\\AllPrintingsCSVFiles\\legalities.csv',SAMPLE_SIZE=-1);")
+dbExecute(Magic_DB, "CREATE OR REPLACE TABLE MTG.Meta AS SELECT * FROM read_csv_auto('..\\MTG_Database\\data\\sqllite_data\\AllPrintingsCSVFiles\\meta.csv',SAMPLE_SIZE=-1);")
+dbExecute(Magic_DB, "CREATE OR REPLACE TABLE MTG.Rulings AS SELECT * FROM read_csv_auto('..\\MTG_Database\\data\\sqllite_data\\AllPrintingsCSVFiles\\rulings.csv',SAMPLE_SIZE=-1);")
+dbExecute(Magic_DB, "CREATE OR REPLACE TABLE MTG.Set_translations AS SELECT * FROM read_csv_auto('..\\MTG_Database\\data\\sqllite_data\\AllPrintingsCSVFiles\\set_translations.csv',SAMPLE_SIZE=-1);")
+dbExecute(Magic_DB, "CREATE OR REPLACE TABLE MTG.Sets AS SELECT * FROM read_csv_auto('..\\MTG_Database\\data\\sqllite_data\\AllPrintingsCSVFiles\\sets.csv',SAMPLE_SIZE=-1);")
+dbExecute(Magic_DB, "CREATE OR REPLACE TABLE MTG.Tokens AS SELECT * FROM read_csv_auto('..\\MTG_Database\\data\\sqllite_data\\AllPrintingsCSVFiles\\tokens.csv',SAMPLE_SIZE=-1);")
 
-# dbExecute(Magic_DB, "CREATE UNIQUE INDEX id ON set(id)")
+con <- dbGetQuery(Magic_DB, "SELECT * FROM MTG.Meta")
 
-# #transforming json file into table
-
-# dbExecute(Magic_DB, "json(")
-
-# dbExecute(Magic_DB, "CREATE OR REPLACE TABLE set(id INTEGER PRIMARY KEY, 
-#                                                  Name VARCHAR, 
-#                                                  Release_Date DATE)")
-# dbExecute(Magic_DB, "Create OR RELACE TABLE card(id INTEGER PRIMARY KEY, 
-#                                                  Name VARCHAR")
-
-# dbExecute(Magic_DB, "INSERT INTO set VALUES (1,'Alpha', '1993-08-05'), 
-#                                             (2, 'Beta', '1993-10-04'), 
-#                                             (3, 'Unlimited', '1993-12-01')")
-
-
-
-
-MTG_View <- dbGetQuery(Magic_DB, "SELECT * FROM set")
-
-# View(MTG_View)
-
+con_1 <- dbGetQuery(Magic_DB, "DESCRIBE MTG.Cards;")
+print(con_1)
+ 
 dbDisconnect(Magic_DB, shutdown = TRUE)
